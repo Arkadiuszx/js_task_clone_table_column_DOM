9c637a905036a3bb5aa7babf0ec7a2f7d5aca1c0
Arkadiusz <stefanski.a00@gmail.com>
2024-02-03 05:59:06 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const table = document.querySelector("table");
+    const headerRow = table.querySelector("thead tr");
+    const headerPosition = headerRow.getElementsByTagName("th")[1];
+    const clonedHeaderPosition = headerPosition.cloneNode(true);
+    const salaryHeader = headerRow.querySelector("th:last-child");
+    salaryHeader.parentNode.insertBefore(clonedHeaderPosition, salaryHeader);
+    const footerRow = table.querySelector("tfoot tr");
+    const footerPosition = footerRow.getElementsByTagName("th")[1];
+    const clonedFooterPosition = footerPosition.cloneNode(true);
+    const salaryFooter = footerRow.querySelector("th:last-child");
+    salaryFooter.parentNode.insertBefore(clonedFooterPosition, salaryFooter);
+    const rows = Array.from(table.getElementsByTagName("tbody")[0].getElementsByTagName("tr"));
+    rows.forEach((row)=>{
+        const position = row.getElementsByTagName("td")[1];
+        const clonedPosition = position.cloneNode(true);
+        const salaryCell = row.getElementsByTagName("td")[4];
+        salaryCell.parentNode.insertBefore(clonedPosition, salaryCell);
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
